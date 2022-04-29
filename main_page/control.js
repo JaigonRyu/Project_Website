66fcc7e590bb0ee5b2bec0da9960b5ec0e7f2827
jhanshaw-lab <jacob.soccer2002@gmail.com>
2022-04-28 20:53:18 -0700

    more change

--- main_page/control.js
+++ main_page/control.js
@@ -1,35 +1,5 @@
 
-
-let astronaut = document.querySelector('.astronaut');
-let moveBy = 20;
-
-
-window.addEventListener('load', () => {
-    astronaut.style.position = 'absolute';
-    astronaut.style.left = 0;
-    astronaut.style.top = 0;
-});
-
-
-window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'ArrowLeft':
-            astronaut.style.left = parseInt(astronaut.style.left) - moveBy + 'px';
-            break;
-        case 'ArrowRight':
-            astronaut.style.left = parseInt(astronaut.style.left) + moveBy + 'px';
-            break;
-        case 'ArrowUp':
-            astronaut.style.top = parseInt(astronaut.style.top) - moveBy + 'px';
-            break;
-        case 'ArrowDown':
-            astronaut.style.top = parseInt(astronaut.style.top) + moveBy + 'px';
-            break;
-    }
-
-
-});
-
+//javascript for the modal button with instructions
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
@@ -66,3 +36,71 @@ function closeModal(modal) {
   modal.classList.remove('active')
   overlay.classList.remove('active')
 }
+
+
+//break for draggable astronaut
+const d = document.getElementsByClassName("astronaut");
+
+for (let i = 0; i < d.length; i++) {
+  d[i].style.position = "relative";
+}
+
+function filter(e) {
+  let target = e.target;
+
+  if (!target.classList.contains("astronaut")) {
+    return;
+  }
+
+  target.moving = true;
+
+  //NOTICE THIS 👇 Check if Mouse events exist on users' device
+  if (e.clientX) {
+    target.oldX = e.clientX; // If they exist then use Mouse input
+    target.oldY = e.clientY;
+  } else {
+    target.oldX = e.touches[0].clientX; // Otherwise use touch input
+    target.oldY = e.touches[0].clientY;
+  }
+  //NOTICE THIS 👆 Since there can be multiple touches, you need to mention which touch to look for, we are using the first touch only in this case
+
+  target.oldLeft = window.getComputedStyle(target).getPropertyValue('left').split('px')[0] * 1;
+  target.oldTop = window.getComputedStyle(target).getPropertyValue('top').split('px')[0] * 1;
+
+  document.onmousemove = dr;
+  //NOTICE THIS 👇
+  document.ontouchmove = dr;
+  //NOTICE THIS 👆
+
+  function dr(event) {
+    event.preventDefault();
+
+    if (!target.moving) {
+      return;
+    }
+    //NOTICE THIS 👇
+    if (event.clientX) {
+      target.distX = event.clientX - target.oldX;
+      target.distY = event.clientY - target.oldY;
+    } else {
+      target.distX = event.touches[0].clientX - target.oldX;
+      target.distY = event.touches[0].clientY - target.oldY;
+    }
+    //NOTICE THIS 👆
+
+    target.style.left = target.oldLeft + target.distX + "px";
+    target.style.top = target.oldTop + target.distY + "px";
+  }
+
+  function endDrag() {
+    target.moving = false;
+  }
+  target.onmouseup = endDrag;
+  //NOTICE THIS 👇
+  target.ontouchend = endDrag;
+  //NOTICE THIS 👆
+}
+document.onmousedown = filter;
+//NOTICE THIS 👇
+document.ontouchstart = filter;
+//NOTICE THIS 👆
